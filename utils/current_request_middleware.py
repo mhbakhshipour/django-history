1945125e1f7a493149863b0632cf8a1f9de0ed82
mh bakhshipour <41994936+mhbakhshipour@users.noreply.github.com>
2023-06-07 11:18:53 +0330

    Update current_request_middleware.py for log all users (user admin panel included)

--- utils/current_request_middleware.py
+++ utils/current_request_middleware.py
@@ -1,6 +1,24 @@
-from threading import current_thread
+from threading import local
+from django.utils.deprecation import MiddlewareMixin
+
+_thread_locals = local()
 
 
 def current_request():
-    _requests = {}
-    return _requests.get(current_thread().ident, None)
+    return getattr(_thread_locals, 'request', None)
+
+
+class RequestMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        _thread_locals.request = request
+
+    def process_response(self, request, response):
+        # Clean up the request after the response is processed
+        if hasattr(_thread_locals, 'request'):
+            del _thread_locals.request
+        return response
+
+    def process_exception(self, request, exception):
+        # Clean up the request if an exception occurs
+        if hasattr(_thread_locals, 'request'):
+            del _thread_locals.request
